c618e2bfffde837956a8fe189b322049ffdccece
Chase McCoy <chase.a.mccoy@outlook.com>
2025-10-09 16:40:30 -0700

    change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://chaseamccoy.github.io/personal-website/',
-    base: '/personal-website/', // ✅ critical for GitHub Pages
+    site: 'https://chasemccoy.dev', // your custom domain
+    base: '/', // root path
     outDir: './dist',
-  });
+});
